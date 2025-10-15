62ecc33c37649747d34e1d6694a1fb6a5e648438
coutinhoAlves <couto.alves@outlook.com>
2025-10-15 13:44:24 -0400

    Organizando componentes em pastas padronizadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import './styles/theme.css';
 import './styles/global.css';
+
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
+
 export const App = () => {
   console.log('App component rendered');
   return (
-    <div>
-      <Heading />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-        voluptatibus laborum corrupti similique dolorum. Totam minima nobis
-        necessitatibus maxime ex. Temporibus, nam repudiandae! Doloribus
-        distinctio eveniet nesciunt, quisquam illum expedita.
-      </p>
-    </div>
+    <>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+    </>
   );
 };
