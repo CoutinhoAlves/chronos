fb5988eb4e473b94779ff386039c774abac2b623
coutinhoAlves <couto.alves@outlook.com>
2025-10-15 22:19:54 -0400

    editando fotos

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Container } from './components/Container';
-import { Heading } from './components/Heading';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
 
 export const App = () => {
   console.log('App component rendered');
   return (
     <>
       <Container>
-        <Heading>LOGO</Heading>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
       </Container>
     </>
   );
